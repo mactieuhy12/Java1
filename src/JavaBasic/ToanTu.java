36d80e8174462704c2d6da286928e5b756b61a99
mactieuhy12 <Hoanghaiyen12a12062002@gmail.com>
2024-02-29 11:47:31 +0700

    commit

---
+++ src/JavaBasic/ToanTu.java
@@ -0,0 +1,13 @@
+package JavaBasic;
+
+import java.sql.SQLOutput;
+
+public class ToanTu {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println("Tong cua a va b la: " +(a +b));
+        System.out.println("Hieu cua a va b la: " +(a - b));
+    }
+
+}
